f370f0665b8dbee589ba74b5bee8fc9ff276d5b8
Anas-kenawi <kenawi3@gmail.com>
2022-10-21 09:41:33 +0000

    versione1

---
+++ esercizio2.py
@@ -0,0 +1,18 @@
+#creo una lista e la stampo, la lista può avere doppioni , può contenere variabili di diverso tipo
+perry = ["carote", "sedano", "cipolla"]
+print(perry)
+#creo una lista e ne stampo la lunghezza
+lista1 = 12, 45, 77, 87, 34, 9
+print(len(lista1))
+#stampo il tipo di lista
+print(type(lista1))
+#stampo uno o più dei valori all'interno della lista(il primo è 0 e l'ultimo è -1)
+print(lista1[2])
+print(lista1[-1])
+print(lista1 [2:4])
+print(lista1[:4])
+print(lista1[2:])
+#controllare se in una lista c'è un valore
+if 77 in lista1:
+    print( "77 le gambe delle donne")
+
